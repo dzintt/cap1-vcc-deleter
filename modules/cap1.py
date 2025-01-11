834d5937a3e0c999c36016d1a94e42f380306818
DZ <ansontan635@gmail.com>
2025-01-10 19:35:15 -0800

    backup get accounts method

--- modules/cap1.py
+++ modules/cap1.py
@@ -81,8 +81,18 @@ class CapitalOneVCCDeleter:
         self.session.headers["accept"] = "application/json;v=1"
         response = self.session.get("https://myaccounts.capitalone.com/web-api/private/1491939/edge/customer/profile/preferences")
         result = response.json()
+        if "accountDisplayOrder" not in result:
+            return self.get_accounts_v2()
         print("Found", len(result["accountDisplayOrder"]), "cards")
         self.card_ids = result["accountDisplayOrder"]
+        return self.card_ids
+    
+    def get_accounts_v2(self) -> list[str]:
+        self.session.headers["accept"] = "application/json;v=1"
+        response = self.session.get("https://myaccounts.capitalone.com/web-api/protected/636178/customer-accounts?versionUpgrade=false")
+        result = response.json()
+        print("Found", len(result["entries"]), "cards")
+        self.card_ids = [entry["accountReferenceId"] for entry in result["entries"]]
         return self.card_ids
 
     def get_all_vccs(self, search: str = None) -> list[VCCEntry]:
